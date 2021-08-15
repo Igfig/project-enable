05e339fb83e5ff9db35020def57fe0347aba550f
ifich <ira.fich@gmail.com>
2021-08-15 19:23:47 -0400

    barely functional version of english/dine split. needs an enormous amount of cleanup

--- csv_to_json.py
+++ csv_to_json.py
@@ -2,17 +2,14 @@ import json
 import re
 import os.path
 import itertools
+from functools import partialmethod
 
 from unidecode import unidecode
 
 # This is the newest version, use this
 
-# TODO we need separate variants for english and dine
-# because some dine words have multiple english ones. Notably akeelyei
-# FIXME it's using the normalized dine words instead of the accented ones on the dine side of the dictionary
-
 ENGLISH = "english"
-NAVAJO = "navajo"
+DINE = "dine"
 AUDIO_PATH = "/audio/"
 
 # TODO add line numbers
@@ -21,9 +18,10 @@ defaults = ("English word", "Diné word", "No definition provided",
             "No definition provided", None, None)
 
 eng_audio = os.listdir(os.curdir + AUDIO_PATH + ENGLISH)
-dine_audio = os.listdir(os.curdir + AUDIO_PATH + NAVAJO)
+dine_audio = os.listdir(os.curdir + AUDIO_PATH + DINE)
 
-reuse_pattern = re.compile(r"@(\d)$")
+ref_pattern = re.compile(r"@(\d)$")
+normalization_table = str.maketrans(" ", "-", "'(),.")
 
 
 # surprised there isn't something that does this already
@@ -51,13 +49,15 @@ def first(iterable, func):
 
 
 def normalize(accented_string):
-	return unidecode(accented_string).replace("'", "").lower()
+	return unidecode(accented_string).translate(normalization_table).lower()
 
 
-def get_audio(lang, word):
-	audio_path = normalize(word) + ".mp3"
-	if audio_path in eng_audio:
-		return AUDIO_PATH + lang + "/" + audio_path
+def get_audio(lang, word, filename, directory):
+	if not filename:
+		filename = normalize(word)
+	audio_path = filename + "-" + lang + ".m4a"
+	if audio_path in directory:
+		return AUDIO_PATH + audio_path
 	return ""
 
 
@@ -66,15 +66,25 @@ def get_field(e, field):
 		return getattr(e, field)
 	except AttributeError:
 		return e[field]
-	
-	
+
+
 def get_reference(text):
-	return re.match(reuse_pattern, text).group(1)
+	return re.match(ref_pattern, text).group(1)
+
+
+def keyfunc_eng(r):
+	return normalize(r.english)
+
+
+def keyfunc_dine(r):
+	return normalize(r.navajo)
+
 
 class Row:
 	def __init__(self, row):
 		(self.english,
-		 self.variant,
+		 self.variant_eng,
+		 self.variant_navajo,
 		 self.navajo,
 		 self.def_eng,
 		 self.def_navajo,
@@ -89,39 +99,87 @@ class Row:
 		 self.audio_english,
 		 self.audio_navajo
 		 ) = [x.strip() for x in row]
+		self.normalized_eng = normalize(self.english)
+		self.normalized_navajo = normalize(self.navajo)
+
+class RawRows:
+	def __init__(self):
+		self.eng = {}
+		self.dine = {}
 
 
+raw_rows = RawRows()
+
+
+class SeeAlso:
+	def __init__(self, word):
+		self.word = word
+		self.normalized = normalize(word)
+	
+	@staticmethod
+	def get_see_also(attr: str):
+		def get_see_also_row(row: Row):
+			return [SeeAlso(sa).dump() for sa in getattr(row, attr).split(", ") if sa]
+		
+		return get_see_also_row
+	
+	def dump(self):
+		return self.__dict__
+
+class SeeAlsos:
+	def __init__(self):
+		self.entries = {}
+	
+	def add_row(self, attr, row):
+		see_also = getattr(row, attr)
+		ref_match = ref_pattern.match(see_also)
+		if ref_match:
+			# it's an @ reference
+			see_also = getattr(raw_rows.eng[(row.english, ref_match.group(1))], attr)
+		
+		for word in see_also.split(", "):
+			if word:
+				self.entries[normalize(word)] = word
+		
+	def add(self, attr):
+		return partialmethod(self.add_row, attr)
+	
+	def dump(self):
+		return [{"word": word, "normalized": normalized} for (normalized, word) in self.entries.items()]
+	
+	
 class Dictionary:
 	def __init__(self, lines):
 		self.english = {}
 		self.navajo = {}
 		
-		rows = [Row(line.strip("\n").split(CSV_DIVIDER)) for line in lines]
+		unsorted_rows = [Row(line.strip("\n").split(CSV_DIVIDER)) for line in lines]
+		# sort to ensure that we don't reference variants we haven't seen yet
+		rows = sorted(unsorted_rows, key=lambda r: normalize(r.english) + r.variant_eng)
 		
-		eng_rows = itertools.groupby(rows, lambda r: r.english)
-		navajo_rows = itertools.groupby(rows, lambda r: r.navajo)
+		# I think doing something like this and establishing a lookup would make things
+		# much easier when trying to get variants
+		raw_rows.eng = {(row.english, row.variant_eng): row for row in
+		                sorted(rows, key=keyfunc_eng)}
+		raw_rows.dine = {(row.navajo, row.variant_navajo): row for row in
+		                 sorted(rows, key=keyfunc_dine)}
 		
-		for (eng_word, word_rows) in eng_rows:
-			key = normalize(eng_word)
-			entry = EngToDine(key)
+		eng_rows = itertools.groupby(sorted(rows, key=keyfunc_eng), keyfunc_eng)
+		navajo_rows = itertools.groupby(sorted(rows, key=keyfunc_dine), keyfunc_dine)
+		
+		for (eng_key, word_rows) in eng_rows:
+			entry = EngToDine(eng_key)
 			
-			# sort to ensure that we don't reference variants we haven't seen yet
-			sorted_word_rows = sorted(word_rows, key=lambda r: r.variant)
-			
-			for row in sorted_word_rows:
+			for row in word_rows:
 				entry.add(row)
-			self.english[eng_word] = entry
+			self.english[eng_key] = entry
+		
+		for (navajo_key, word_rows) in navajo_rows:
+			entry = DineToEng(navajo_key)
 			
-		for (navajo_word, word_rows) in navajo_rows:
-			key = normalize(navajo_word)
-			entry = DineToEng(key)
-			
-			# sort to ensure that we don't reference variants we haven't seen yet
-			sorted_word_rows = sorted(word_rows, key=lambda r: r.variant)
-			
-			for row in sorted_word_rows:
+			for row in word_rows:
 				entry.add(row)
-			self.navajo[navajo_word] = entry
+			self.navajo[navajo_key] = entry
 	
 	def dump(self, out):
 		english = {k: e.dump() for (k, e) in self.english.items()}
@@ -134,14 +192,15 @@ class Dictionary:
 class EngToDine:
 	keys = {
 		"word": "navajo",
-		"variant": None,  # None means "just use the key". e.g. here look for "variant"
-		"literal": None,
+		"variant": "variant_eng",
+		"literal": None,  # None means "just use the key". e.g. here look for "literal"
 		"comment": None,
-		"see_also": "see_also_navajo"
+		"see_also": SeeAlso.get_see_also("see_also_navajo")
 	}
 	
 	def __init__(self, word):
-		self.word = word
+		self.word = ""
+		self.normalized = word
 		self.definition = ""
 		self.sentence = ""
 		self.phonetic = ""
@@ -151,16 +210,17 @@ class EngToDine:
 		self.translations = Translations("def_navajo", self.keys)
 	
 	def add(self, row):
-		if not reuse_pattern.match(row.def_eng):
+		self.word = row.english
+		if not ref_pattern.match(row.def_eng):
 			self.definition = row.def_eng
-		if not reuse_pattern.match(row.sentence_eng):
+		if not ref_pattern.match(row.sentence_eng):
 			self.sentence = row.sentence_eng
-		if not reuse_pattern.match(row.phonetic_eng):
+		if not ref_pattern.match(row.phonetic_eng):
 			self.phonetic = row.phonetic_eng
-		if not reuse_pattern.match(row.see_also_eng):
-			self.see_also = [normalize(sa) for sa in row.see_also_eng.split(", ") if sa]
+		if not ref_pattern.match(row.see_also_eng):
+			self.see_also = SeeAlso.get_see_also("see_also_eng")(row)
 		
-		self.audio = get_audio(ENGLISH, row.english)
+		self.audio = get_audio(ENGLISH, row.english, row.audio_english, eng_audio)
 		
 		self.short_translations.append(row.navajo)
 		self.translations.add(row)
@@ -171,66 +231,172 @@ class EngToDine:
 		return out
 
 
-class DineToEng:
+class Settable:
+	def __init__(self):
+		self.data = {}
+	
+	def set(self, row, source, destination):
+		if not source:
+			source = destination
+		
+		try:
+			# is it callable?
+			value = source(row)
+		except TypeError:
+			# it is not callable
+			value = getattr(row, source)
+		
+		
+		try:
+			ref_match = ref_pattern.match(value)
+			if ref_match:
+				# it's an @ reference
+				value = getattr(raw_rows.eng[(row.english, ref_match.group(1))], source)
+		except TypeError:
+			# it's not a string, actually
+			pass
+		
+		if value:
+			self.data[destination] = value
+
+
+class DineToEng(Settable):
 	keys = {
+		"word": "navajo",
+		"normalized": "normalized_navajo",
+		"definition": "def_navajo",
+		"literal": "literal",
+		"sentence": "sentence_navajo",
+		"phonetic": "phonetic_navajo",
+		# "see_also": SeeAlso.get_see_also("see_also_navajo"),
+		"audio": lambda row: get_audio(DINE, row.english, row.audio_navajo, dine_audio)
+	}
+	keys2 = {
+		"definition": "def_eng",
+		"see_also": SeeAlso.get_see_also("see_also_eng")
+	}
+	keys3 = {
 		"word": "english",
-		"variant": None,  # None means "just use the key". e.g. here look for "variant"
-		"comment": None,
-		"see_also": "see_also_eng"
+		"normalized": "normalized_eng",
+		"variant": "variant_navajo",
+		"comment": None
 	}
 	
 	def __init__(self, word):
+		super().__init__()
 		self.word = word
-		self.definition = ""
-		self.literal = ""
-		self.sentence = ""
-		self.phonetic = ""
-		self.audio = ""
-		self.see_also = []
+		# self.see_also = []
+		self.see_also = SeeAlsos()
 		self.short_translations = []
-		self.translations = Translations("def_eng", self.keys)
+		# self.translations = Translations2("def_eng", "see_also_eng", self.keys3)
+		self.translations = {}
 	
 	def add(self, row):
-		if not reuse_pattern.match(row.def_navajo):
-			self.definition = row.def_navajo
-		if not reuse_pattern.match(row.literal):
-			self.literal = row.literal
-		if not reuse_pattern.match(row.sentence_navajo):
-			self.sentence = row.sentence_navajo
-		if not reuse_pattern.match(row.phonetic_navajo):
-			self.phonetic = row.phonetic_navajo
-		if not reuse_pattern.match(row.see_also_navajo):
-			self.see_also = [normalize(sa) for sa in row.see_also_navajo.split(", ") if sa]
-		
-		self.audio = get_audio(NAVAJO, row.navajo)
+		for (key, source) in self.keys.items():
+			self.set(row, source, key)
 		
 		self.short_translations.append(row.english)
-		self.translations.add(row)
+		# self.translations.add(row)
+		self.add_translation(row)
+		self.see_also.add("see_also_navajo")
+	
+	def add_translation(self, row):
+		definition = getattr(row, "def_eng")
+		translation = self.translations.setdefault(definition, Translation(self.keys, row))
+		translation.add(row, self.keys3)
+		translation.see_also.add_row("see_also_eng", row)
 	
 	def dump(self):
-		out = {k: v for (k, v) in self.__dict__.items() if v}
-		out["translations"] = self.translations.dump()
+		out = {k: v for (k, v) in self.data.items() if v}
+		out["short_translations"] = self.short_translations
+		# out["translations"] = self.translations.dump()
+		out["translations"] = [t.dump() for t in self.translations.values()]
 		return out
+
+
+# class Translations2:
+# 	def __init__(self, def_key, see_also_key, sub_keys):
+# 		self.def_key = def_key
+# 		self.see_also_key = see_also_key
+# 		self.keys = {
+# 			"definition": def_key
+# 		}
+# 		self.sub_keys = sub_keys
+# 		self.entries = {}
+#
+# 	def add(self, row):
+# 		definition = getattr(row, self.def_key)
+#
+# 		translation = self.entries.setdefault(definition, Translation(self.keys, row))
+#
+# 		# # you may end up using this snippet for that, instead of setdefault
+# 		# if definition not in self.entries:
+# 		# 	self.entries[definition] = Translation(self.keys, row)
+# 		# translation = self.entries[definition]
+#
+# 		translation.add(row, self.sub_keys)
+# 		translation.see_also.add_row(self.see_also_key, row)
+#
+#
+# 	def dump(self):
+# 		return [e.dump() for e in self.entries.values()]
+
+
+class Translation(Settable):
+	def __init__(self, keys, row):
+		super().__init__()
+		self.see_also = SeeAlsos()
+		self.entries = []
+		for (destination, source) in keys.items():
+			self.set(row, source, destination)
+	
+	def add(self, row, sub_keys):
+		self.entries.append(TranslationEntry(sub_keys, row))
+	
+	def dump(self):
+		out = {k: v for (k, v) in self.data.items() if v}
+		out["see_also"] = self.see_also.dump()
+		out["entries"] = [e.dump() for e in self.entries]
+		return out
+	
+	
+class TranslationEntry(Settable):
+	def __init__(self, keys, row):
+		super().__init__()
+		self.data = {}
+		for (destination, source) in keys.items():
+			self.set(row, source, destination)
+	
+	def dump(self):
+		return {k: v for (k, v) in self.data.items() if v}
+
 
 
 class VariantHolder:
 	"""Base class only, do not instantiate"""
-
+	
 	def __init__(self, definition, keys):
 		self.definition = definition
 		self.entries = []
 		self.keys = keys
-		
+	
+	# TODO use raw_rows
 	def find_variant(self, cell):
-		reuse_match = reuse_pattern.match(cell)
-		if reuse_match:
-			# pick out the appropriate entry
-			variant_to_use = reuse_match.group(1)
-			filter_variant = self.get_variant_filter(variant_to_use)
-			return first(self.entries, filter_variant)
-		else:
-			# no variant specified for reuse, so tell the calling method to just do default behaviour
+		try:
+			ref_match = ref_pattern.match(cell)
+		except TypeError:
+			# in some cases it can be an empty list. TODO are we sure this is the right behaviour?
 			return None
+		if not ref_match:
+			# no variant specified for reuse, so tell the calling method to just do default
+			# behaviour
+			return None
+		
+		# pick out the appropriate entry
+		ref_index = ref_match.group(1)
+		filter_variant = self.get_variant_filter(ref_index)
+		b = first(self.entries, filter_variant)
+		return b
 	
 	def get_def(self, row):
 		return getattr(row, self.definition)
@@ -240,6 +406,7 @@ class VariantHolder:
 		def filter_variant(e):
 			field = get_field(e, "variant")
 			return field == variant_to_use
+		
 		return filter_variant
 
 
@@ -249,15 +416,16 @@ class Translations(VariantHolder):
 		
 		if not entry:
 			# there's no @X in the definition, so add a new entry
-			entry = TranslationWords(self.get_def(row), row.variant, self.keys)
+			entry = TranslationWords(self.get_def(row), getattr(row, self.keys["variant"]),
+			                         self.keys)
 			self.entries.append(entry)
 		
 		entry.add(row)
-
+	
 	def dump(self):
 		return [e.dump() for e in self.entries]
-	
-	
+
+
 class TranslationWords(VariantHolder):
 	def __init__(self, definition, variant, keys):
 		self.variant = variant
@@ -270,18 +438,24 @@ class TranslationWords(VariantHolder):
 		row_key = self.keys[key]
 		if not row_key:
 			row_key = key
-		value = getattr(row, row_key)
-		
+			
+		try:
+			# is it callable?
+			value = row_key(row)
+		except TypeError:
+			# it is not callable
+			value = getattr(row, row_key)
+				
 		entry = self.find_variant(value)
 		if entry:
 			return entry[row_key]
 		else:
 			return value
-		
+	
 	def dump(self):
 		dump_dict = self.__dict__.copy()
 		del dump_dict["keys"]  # we don't want to include the keys attr in the dump
-		del dump_dict["variant"]  # we don't want to include the keys attr in the dump=
+		del dump_dict["variant"]  # ditto
 		dump_dict["entries"] = [{k: v for (k, v) in entry.items() if v}
 		                        for entry in dump_dict["entries"]]
 		return dump_dict
