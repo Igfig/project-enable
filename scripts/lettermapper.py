40fe3c2b66f632fb289fcbd2bf00720ac5f4dd56
ifich <ira.fich@gmail.com>
2022-04-18 11:20:38 -0400

    update lettermapper script to reduce false positives

--- scripts/lettermapper.py
+++ scripts/lettermapper.py
@@ -23,24 +23,49 @@ mapping = {
 	'^': 'Ę́',
 	'&': 'Í',
 	'*': 'Į',
-	'(': 'Į́',
-	')': 'Ó',
+	# '(': 'Į́', # these two letters never show up in the word list, but parens often do
+	# ')': 'Ó',
 	'_': 'Ǫ',
 	'+': 'Ǫ́',
 	'{': 'Ł',
 	'}': 'Ń'
 }
+dine_chars = mapping.values()
 
 table = str.maketrans(mapping)
 
+skip_columns = [0, 1, 2, 4, 6, 7, 8, 9, 10]
+
+def translate_cell(cell, cells):
+	if cells.index(cell) in skip_columns:
+		# this is a column that we shouldn't translate
+		return cell
+	
+	if not set(cell).isdisjoint(dine_chars):
+		# this cell is already translated
+		return cell
+	
+	if re.match("^[\d@]*$", cell):
+		# this cell is just numeric (or has an @ symbol, which is used as an indicator)
+		return cell
+	
+	translated = cell.translate(table)
+	repunctuated = re.sub(r"\bĮ́(.+)Ó\b", r"(\1)", translated)
+	return repunctuated
+
+def translate_line(line):
+	split = line.split("\t")
+	translated = [translate_cell(c, split) for c in split]
+	return "\t".join(translated)
 
 def main():
 	with open("./tomap.csv", encoding='utf-8') as csv:
-		translated = [line.translate(table) for line in csv]
-		repunctuated = [re.sub(r"\bĮ́(.+)Ó\b", r"(\1)", t) for t in translated]
+		translated = [translate_line(line) for line in csv]
 		
-		for t in repunctuated:
+	with open("./mapped.csv", "w", encoding='utf-8') as out:
+		for t in translated:
 			print(t, end="")
+			out.write(t)
 
 
 if __name__ == "__main__":
